d76deb58bcc2706a24a8af01346617aa2ba1398d
loudagnese <louis.dagnese@gmail.com>
2022-08-06 11:27:59 -0700

    pull updates from main: main.handlebars

--- server.js
+++ server.js
@@ -1,2 +1,23 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path');
+
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+
+const hbs = exphbs.create({});
+
+// Sets up the Express App
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Set Handlebars as the default template engine.
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+// Start the server to begin listening
+app.listen(PORT, () => {
+    console.log(`Server listening on: http://localhost:${PORT}`);
+});
+
 
