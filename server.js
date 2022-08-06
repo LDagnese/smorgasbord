852b6a68acca8cefc97bc61396dd8952861662d9
loudagnese <louis.dagnese@gmail.com>
2022-08-06 16:46:43 -0700

    set up template for homepage

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
+const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
-const path = require('path');
+const hbs = exphbs.create({});
 
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
-
-const hbs = exphbs.create({});
 
 // Sets up the Express App
 const app = express();
@@ -14,6 +13,8 @@ const PORT = process.env.PORT || 3001;
 // Set Handlebars as the default template engine.
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+
+app.use(routes);
 
 // Start the server to begin listening
 app.listen(PORT, () => {
