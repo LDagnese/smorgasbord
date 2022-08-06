767753a3c73d8e0561b04ecfc697a4359a495035
lillielove <lillielove@gmail.com>
2022-08-06 11:36:53 -0700

    added all models, initialized sequelize db connection in config/connection.js

---
+++ models/Cart.js
@@ -0,0 +1,46 @@
+// import important parts of sequelize library
+const { Model, DataTypes } = require("sequelize");
+// import our database connection from config.js
+const sequelize = require("../config/connection");
+
+class Cart extends Model {}
+
+Cart.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: "user",
+                key: "id",
+            },
+        },
+        restaurant_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: "restaurant",
+                key: "id",
+            },
+        },
+        dish_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: "dish",
+                key: "id",
+            },
+        },
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: "cart",
+    }
+);
+
+module.exports = Cart;
---
+++ models/Dish.js
@@ -0,0 +1,55 @@
+// import important parts of sequelize library
+const { Model, DataTypes } = require("sequelize");
+// import our database connection from config.js
+const sequelize = require("../config/connection");
+
+class Dish extends Model {}
+
+Dish.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        price: {
+            type: DataTypes.DECIMAL,
+            allowNull: false,
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1],
+            },
+        },
+        vegetarian: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+        },
+        gluten_free: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+        },
+        restaurant_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: "restaurant",
+                key: "id",
+            },
+        },
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: "dish",
+    }
+);
+
+module.exports = Dish;
