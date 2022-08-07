88099f57c99a0d0b1beb5cdbeb3579f4b13a9d34
janethxuiz <janethruiz152@gmail.com>
2022-08-06 18:14:44 -0700

    added start fucntions for the cart seeds file and module export

---
+++ seeds/cart-seeds.js
@@ -0,0 +1,33 @@
+const { Cart } = require('../models');
+
+const cartdata = [
+    {
+        user_id:
+        restaurant_id:
+        dish_id:
+    },
+    {
+        user_id:
+        restaurant_id:
+        dish_id:
+    },
+    {
+        user_id:
+        restaurant_id:
+        dish_id:
+    },
+    {
+        user_id:
+        restaurant_id:
+        dish_id:
+    },
+    {
+        user_id:
+        restaurant_id:
+        dish_id:
+    }
+];
+
+const seedComments = () => Comment.bulkCreate(commentdata);
+
+module.exports = seedComments;
