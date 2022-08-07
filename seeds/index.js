84c407136f28728ae06fa96571cacf33099873f1
janethxuiz <janethruiz152@gmail.com>
2022-08-06 17:44:34 -0700

    seeds files added to connec t to index seed file

--- seeds/index.js
+++ seeds/index.js
@@ -1,1 +1,7 @@
+const seedCart = require('./cart-seeds');
+const seedDish = require('./dish-seeds');
+const seedRestaurant = require('./restaurant-seeds');
+const seedUser = require('./user-seeds');
+
+const sequelize = require('../config/connection');
 
