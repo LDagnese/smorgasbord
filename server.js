341ee937e8d0a40d208780f79bcd2b46ba3fd56e
loudagnese <louis.dagnese@gmail.com>
2022-08-06 17:25:36 -0700

    fix stylesheet link

--- server.js
+++ server.js
@@ -14,6 +14,8 @@ const PORT = process.env.PORT || 3001;
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(routes);
 
 // Start the server to begin listening
