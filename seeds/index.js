f466f9ab1995b79527ac4f5ed12416c9f929f106
janethxuiz <janethruiz152@gmail.com>
2022-08-06 17:58:57 -0700

    seeding all files together

--- seeds/index.js
+++ seeds/index.js
@@ -5,3 +5,23 @@ const seedUser = require('./user-seeds');
 
 const sequelize = require('../config/connection');
 
+const seedAll = async () => {
+    await sequelize.sync({ force: true });
+    console.log('--------------');
+
+    await seedCart();
+    console.log('--------------');
+
+    await seedDish();
+    console.log('--------------');
+
+    await seedRestaurant();
+    console.log('--------------');
+
+    await seedUser();
+    console.log('--------------');
+
+    process.exit(0);
+};
+
+seedAll();
