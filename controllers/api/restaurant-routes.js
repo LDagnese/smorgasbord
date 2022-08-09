b6297585f6c75bf6962d50a34691250c835a8fe2
lillielove <lillielove@gmail.com>
2022-08-08 19:42:03 -0700

    seeds added, model adjusted

--- controllers/api/restaurant-routes.js
+++ controllers/api/restaurant-routes.js
@@ -1,4 +1,39 @@
 const router = require("express").Router();
-const { Restaurant } = require('../../models');
+const { Restaurant, Dish } = require("../../models");
+
+// GET all restaurants
+router.get("/restaurant", (req, res) => {
+    Restaurant.findAll().then((dbRestaurantData) => {
+        const restaurants = dbRestaurantData.map((restaurant) =>
+            restaurant.get({ plain: true })
+        );
+        res.render("/", restaurants);
+    });
+});
+
+// GET 1 restaurant, display all dishes
+router.get("/restaurant/:id", (req, res) => {
+    Restaurant.findOne({
+        where: {
+            id: req.params.id,
+        },
+        include: [
+            {
+                model: Dish,
+                attributes: [
+                    "id",
+                    "price",
+                    "description",
+                    "name",
+                    "vegetarian",
+                    "gluten_free",
+                ],
+            },
+        ],
+    }).then((dbRestaurantData) => {
+        const restaurant = dbRestaurantData.get({ plain: true });
+        res.render("/", restaurant);
+    });
+});
 
 module.exports = router;
--- models/Restaurant.js
+++ models/Restaurant.js
@@ -6,35 +6,43 @@ const sequelize = require("../config/connection");
 class Restaurant extends Model {}
 
 Restaurant.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        how_expensive: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                contains: "$",
+            },
+        },
+        type_of_food: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        // vegetarian: {
+        //     type: DataTypes.BOOLEAN,
+        //     allowNull: false,
+        // },
+        // gluten_free: {
+        //     type: DataTypes.BOOLEAN,
+        //     allowNull: false,
+        // },
     },
-    name: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    how_expensive: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        contains: "$",
-      },
-    },
-    type_of_food: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-  },
-  {
-    sequelize,
-    freezeTableName: true,
-    underscored: true,
-    modelName: "restaurant",
-  }
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: "restaurant",
+    }
 );
 
 module.exports = Restaurant;
